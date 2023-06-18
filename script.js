be99b1509153c737dd15b64390620d293cf8e881
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-18 10:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function insert_Row() {
  let table = document.getElementById("sampleTable");
 	let tr = table.insertRow(0);
 	let td1 = tr.insertCell(0);
+	td1.textContent ='New Cell1';
     td1 = tr.firstChild;
 	let td2 = tr.insertCell(1);
-	td2 = tr.lastChild;
-	td1.textContent ='New Cell1';
 	td2.textContent ='New Cell2';
+	td2 = tr.lastChild;
 	tr = table.firstChild;
 	table.append(tr);
 }
